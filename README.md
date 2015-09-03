5b6ecda1d39079f946b43fa833a171bdc48cf6e5
MatthiasL <matthias_ludwig@yahoo.com>
2015-09-04 06:32:13 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # dockerode-bluebird
-dockerode bluebird from https://github.com/d6u/Dockership/blob/master/lib/promisified/docker-promisified.js
+dockerode bluebird from https://github.com/d6u/Dockership/blob/master/lib/promisified/docker-promisified.js as discussed in https://github.com/apocas/dockerode/pull/108.
---
+++ index.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+var Bluebird = require('bluebird');
+var Docker = require('dockerode');
+
+Bluebird.promisifyAll(Docker.prototype);
+
+var _getContainer = Docker.prototype.getContainer;
+
+Docker.prototype.getContainer = function () {
+  var container = _getContainer.apply(this, arguments);
+  if (!container.startAsync) {
+    var containerPrototype = Object.getPrototypeOf(container);
+
+    var _exec = containerPrototype.exec;
+    containerPrototype.exec = function (opts, cb) {
+      _exec.call(this, opts, function (err, exec) {
+        if (exec && !exec.startAsync) {
+          Bluebird.promisifyAll(Object.getPrototypeOf(exec));
+        }
+        cb(err, exec);
+      });
+    };
+
+    Bluebird.promisifyAll(containerPrototype);
+  }
+  return container;
+};
+
+module.exports = Docker;
